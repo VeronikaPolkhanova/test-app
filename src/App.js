89543d4b4c2cd928e6c94ee684ffdb236e985d92
Polkhanova Veronika <polkhanova@aheadworks.com>
2023-11-14 12:58:37 +0300

    add timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import Button from "./components/Button";
+import Square from "./components/Square";
 import "./App.css";
 
 function App() {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [timer, setTimer] = useState(5);
+
+  const onStart = () => {
+    setIsDisabled(true);
+    const interval = setInterval(() => setTimer((timer) => timer - 1), 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      setTimer(5);
+      setIsDisabled(false);
+    }, 5000);
+  };
+
   return (
     <div className="App">
-      <Button />
+      <div className="squareContainer">
+        <Square text="1" />
+        <Square text="2" />
+      </div>
+      <Button text={timer} isDisabled={isDisabled} onStart={onStart} />
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <button>Text</button>;
+const Button = ({ text, isDisabled, onStart }) => {
+  return (
+    <button
+      className={isDisabled ? "isDisabled" : ""}
+      disabled={isDisabled}
+      onClick={onStart}>
+      {isDisabled ? text : "Start"}
+    </button>
+  );
 };
 
 export default Button;
